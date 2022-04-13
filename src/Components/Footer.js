ce528c4c247394fd351eb814cfd03711780bde87
Ian <ian@Ians-MacBook-Pro.local>
2022-04-13 12:58:39 -0600

    title and linkedin

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,9 +4,9 @@ function Footer() {
   return (
     <div id="footer">
       <div id="icons">
-        <a href="https://www.linkedin.com/in/ian-ennis-tanstaafl-slatfatf/"><img src="https://imgur.com/8ebtDau.png" alt="linkedin"/></a>
-        <a href="https://github.com/Ian-Ennis"><img src="https://imgur.com/Z7oVssA.png" alt="github"/></a>
-        <a href="https://ian-patrick-ennis.medium.com/"><img src="https://imgur.com/HQBH5uV.png" alt="blog"/></a>
+        <a href="https://www.linkedin.com/in/ian-ennis-tanstaafl-slatfatf/"><img src="https://imgur.com/SFF3aAh.jpeg" alt="linkedin"/></a>
+        <a href="https://github.com/Ian-Ennis"><img src="https://imgur.com/Z7oVssA.jpeg" alt="github"/></a>
+        <a href="https://ian-patrick-ennis.medium.com/"><img src="https://imgur.com/HQBH5uV.jpeg" alt="blog"/></a>
         </div>
         <a href="mailto:ipatrickenn7@gmail.com">ipatrickenn7@gmail.com</a>
     </div>
