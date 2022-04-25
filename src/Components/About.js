c1750ad575c0066425c31a6c39bab9b67d4e6678
Ian <ian@Ians-MacBook-Pro.local>
2022-04-25 17:42:07 -0600

    fixed about me

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,19 +11,21 @@ function About() {
         <div id="about">
           <p>
             Full-stack Web Developer with a curiosity for the world and the
-            universe. Fluent in JavaScript, React/Redux, HTML, CSS, and Ruby on Rails, with an eye
-            for design, a background in Environmental Science and
-            experience in public service.
+            universe. Fluent in JavaScript, React/Redux, HTML, CSS, and Ruby on
+            Rails, with an eye for design, a background in Environmental Science
+            and experience in public service.
           </p>
           <p>
             I have worked with both the National Park and U.S. Fish and Wildlife
-            services, as well as local parks departments, giving back to nature
-            as my goal. I am now expanding the ways I can give back to more
-            communities by exploring the confluence of programming, user
-            experience, and positive intentions. I hope to help to people and
-            planet alike.
+            services, as well as local parks departments, contributing to the
+            health of the natural environment through horticulture.
           </p>
-          <p id="checkout_work">Come check out my work.</p>
+          <p>
+            I am now expanding the ways I can give back to the world by
+            exploring the confluence of programming, user experience, and
+            positive intentions. I intend to continue helping people and planet
+            alike. Come check out my work.
+          </p>
         </div>
       </div>
     </>
