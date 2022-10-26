fea8a47512f8dc4b896f6ee30172852d428489f1
Ian <ipatrickenn7@gmail.com>
2022-10-26 14:14:19 -0600

    About me edit

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,21 +11,23 @@ function About() {
       <div>
         <p>
           Welcome. I am front end web developer with a specialty in ReactJS,
-          alongside skills in TypeScript, JavaScript, HTML5, Vanilla CSS,
-          automated testing with Jest and React Testing Library, and back end integration
-          Ruby on Rails. Additional skills may be viewed under Stack(s).
+          alongside skills in TypeScript, JavaScript, HTML, Vanilla CSS,
+          automated testing with Jest and React Testing Library, and back end
+          integration with Ruby on Rails. Additional skills may be viewed under
+          Stack(s).
         </p>
         <p>
-          I once designed and enhanced aesthetics of outdoor spaces in both
-          rural and urban parks for the benefit of the general public. What once
-          was my pallet within landscape design has evolved into a pallet of front end
-          design. I bring experience collaborating with teams of people who are
-          similarly driven to deliver quality to other people.
+          I once designed and enhanced aesthetics of highly-visible outdoor
+          spaces in both rural and urban parks for the benefit of the general
+          public. What once was my pallet within landscape design has evolved
+          into a pallet of front end design.
         </p>
         <p>
-          I enjoy exploring the confluence of programming, positive intentions,
-          and user experience. Through creating intuitive and enjoyable user
-          interfaces, I can continue to give back others. Come check out my
+          I bring experience collaborating on teams who are highly motivated to
+          solve problems and continually deliver quality to others. I'm driven
+          to explore the confluence of programming, positive intentions, and
+          user experience. By creating intuitive and enjoyable user interfaces,
+          I may continue to give back to those around me. Come check out my
           work.
         </p>
       </div>
