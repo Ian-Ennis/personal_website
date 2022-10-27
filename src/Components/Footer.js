f82cc106e3433794d6745e1e9577492bdca68682
Ian <ipatrickenn7@gmail.com>
2022-10-27 11:18:44 -0600

    Adjusted footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,6 @@ function Footer() {
         <div id="icons">
           <a href="https://www.linkedin.com/in/ian-ennis-tanstaafl-slatfatf/" target="_blank"><img src="https://imgur.com/SFF3aAh.jpeg" alt="linkedin"/></a>
           <a href="https://github.com/Ian-Ennis" target="_blank"><img src="https://imgur.com/Z7oVssA.jpeg" alt="github"/></a>
-          <a href="https://ian-patrick-ennis.medium.com/" target="_blank"><img src="https://imgur.com/HQBH5uV.jpeg" alt="blog"/></a>
         </div>
           <a href="mailto:ipatrickenn7@gmail.com">ipatrickenn7@gmail.com</a>
       </div>
