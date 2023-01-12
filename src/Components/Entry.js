657b7f0c512785d48d090abb2616b55ac07d5979
Ian <ipatrickenn7@gmail.com>
2023-01-12 13:44:47 -0700

    first commit

--- src/Components/Entry.js
+++ src/Components/Entry.js
@@ -1,6 +1,22 @@
 function Entry({ handleEntryClick }) {
   return (
     <div className="entry_screen">
+      <div id="about_me_graphic" className="warp" aria-labelledby="warp-label">
+        <span id="warp-label" className="warp__placeholder">
+          About Me
+        </span>
+
+        <span role="presentation">
+          <span className="warp__0">A</span>
+          <span className="warp__1">b</span>
+          <span className="warp__2">o</span>
+          <span className="warp__3">u</span>
+          <span className="warp__4">t</span>
+          <span className="warp__5"></span>
+          <span className="warp__6">M</span>
+          <span className="warp__7">e</span>
+        </span>
+      </div>
       <img
         id="entry_logo"
         onClick={handleEntryClick}
