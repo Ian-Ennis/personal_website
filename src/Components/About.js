ffc6a411ee9fe396f72f69268b98f52e55ef5aaf
Ian <ipatrickenn7@gmail.com>
2023-01-17 13:11:19 -0700

    about me edit

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,20 +12,20 @@ function About() {
         <p>
           Welcome. I am a front end web developer with a specialty in ReactJS,
           alongside skills in TypeScript, JavaScript, HTML, Vanilla CSS,
-          automated testing, and API development with Ruby on Rails. Additional
+          with automated testing, and API development using Ruby on Rails. Additional
           skills may be viewed under Stack(s).
         </p>
         <p>
-          I bring experience collaborating on highly motivated teams that are dedicated
-          to solving problems and delivering quality. I once designed and
-          enhanced aesthetics of highly-visible outdoor public spaces, and have
-          carried my design pallet with me as I create visually-pleasing and
-          exciting user interfaces.
+          I once designed and enhanced aesthetics of highly-visible outdoor
+          public spaces, and have carried my design pallet forward as I create
+          visually-pleasing and exciting user interfaces. I bring experience
+          collaborating on highly motivated teams that are dedicated to solving
+          problems and delivering quality.
         </p>
         <p>
-          I thoroughly enjoy coding, building both front end and full stack
-          web applications, and continually explore the confluence of
-          programming, user experience, and positive intentions.
+          I thoroughly enjoy coding, building both front end and full stack web
+          applications, and continually explore the confluence of programming,
+          user experience, and positive intentions.
         </p>
         <p>Come have a look at my projects, and get in touch!</p>
       </div>
